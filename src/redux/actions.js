ceef6a5ac5eca7126ffbbcc2e75f34113c703404
Mikolaj Kaliszewski <kaliszewski.mikolaj@gmail.com>
2023-12-02 16:14:57 +0100

    Add comment

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,6 @@
 export const SET_SELECTED_CATEGORY = 'SET_SELECTED_CATEGORY';
 
+// why is it here if not used anywhere?
 export const setSelectedCategory = category => ({
   type: SET_SELECTED_CATEGORY,
   payload: category
